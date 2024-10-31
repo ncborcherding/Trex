470a331f4c3df81e3ed24e7d1c1c79743eb7f062
theHumanBorch <ncborch@gmail.com>
2024-10-31 15:00:47 -0500

    fixing testthat errors again
    Move default assay call for quiet

--- tests/testthat/test-quietTCRgenes.R
+++ tests/testthat/test-quietTCRgenes.R
@@ -3,6 +3,7 @@
 test_that("quietTCRgenes works", {
   
   data("trex_example")
+  SeuratObject::DefaultAssay(trex_example) <- "RNA"
   
   features <- rownames(trex_example@assays$RNA$counts)
   
@@ -12,7 +13,6 @@ test_that("quietTCRgenes works", {
   )
   
   Seurat::VariableFeatures(trex_example) <- features
-  Seurat::DefaultAssay(trex_example) <- "RNA"
   
   trex_example <- quietTCRgenes(trex_example)
   
